b0be62be93e17373af27def7c57931575ada4012
techhubhosting <82578836+techhubhosting@users.noreply.github.com>
2023-08-06 14:27:55 +0300

    Add files via upload

--- dist/js/my.js
+++ dist/js/my.js
@@ -27,3 +27,43 @@ var x = setInterval(function() {
 		document.getElementById("countdown").innerHTML = "¡Ya llegó el 15 de abril!";
 	}
 }); // cierra la función setInterval y la llave de la función anónima
+
+
+
+
+const observers = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('leftslideshow');
+        }
+        else{
+            entry.target.classList.remove('leftslideshow');
+        }
+    })
+})
+
+const hiddenElement = document.querySelectorAll('.leftslide');
+hiddenElement.forEach((el) => observers.observe(el));
+
+var cursor = document.querySelector(".cursor");
+    var cursor2 = document.querySelector(".cursor2");
+    document.addEventListener("mousemove",function(e){
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+    });
+
+	const observer = new IntersectionObserver((entries) =>{
+		entries.forEach((entry)=>{
+			console.log(entry)
+			if (entry.isIntersecting){
+				entry.target.classList.add('show');
+			}
+			else{
+				entry.target.classList.remove('show');
+			}
+		})
+	})
+	
+	const hiddenElements = document.querySelectorAll('.hidden');
+	hiddenElements.forEach((el) => observer.observe(el));
+	
